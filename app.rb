fe9cdbbd777ab5bcdea415cd24ac0fc770bc84ab
Taylor Hudson <taylor.a.hudson@gmail.com>
2013-10-08 15:02:16 -0700

    first commit

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './environments'
+
+class Post < ActiveRecord::Base
+end
+
+# Sinatra routes
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}"
+  else
+    erb :"posts/create"
+  end
+end
+
+get "/posts/create" do
+  @title = "Create post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :'posts/view'
+end
+
